5395a89e06aa1a8018ea75ccdf12f3d8a48010c1
Stephen J. Eglen <sje30@cam.ac.uk>
2024-03-29 20:01:09 +0000

    tidy up plotting

--- v2/example.R
+++ v2/example.R
@@ -29,9 +29,6 @@ curve(1+(x*0), col='blue', main='Plot 1', lwd=4)
 curve(1+x, col='green', main='Plot 2', lwd=3)
 curve(x^2, col='purple', main='Plot 3', xlim=c(-1,1))
 curve(1+x^2+x^3, col='orange', main='Plot 4', xlim=c(-1,1),lwd=2)
-
-
-
 plot(rnorm(1000), main='plot 5')
-hist(runif(100),col='orange', main='plot6')
+hist(runif(100),col='orange', main='plot6 ')
 
